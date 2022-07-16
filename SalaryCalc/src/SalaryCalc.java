e5426c9225d637ada50e6f319003a3c0f8560352
“Hüseyin <hcatalbas@ceng.ktu.edu.tr>
2022-07-16 18:08:52 +0300

    Salary Calculator Project Completed.

---
+++ SalaryCalc/src/SalaryCalc.java
@@ -0,0 +1,6 @@
+public class SalaryCalc {
+    public static void main(String[] args) throws Exception {
+        Employee e1 = new Employee("Hüseyin ÇATALBAŞ", 2000, 45, 1985);
+        e1.printEmployee();
+    }
+}
